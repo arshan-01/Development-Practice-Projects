50e940de3cc511ba12108c7fdcc2b026811edf80
arshan-01 <arshannawaz.one@gmail.com>
2023-08-07 15:28:14 +0500

    Uni search by country name

--- country_search/js/script.js
+++ country_search/js/script.js
@@ -6,12 +6,179 @@ const countryModal = new mdb.Modal(document.getElementById("countryModal"));
 let countryData;
 let currentPage = 1;
 let itemsPerPage = parseInt(selectPerPage.value);
+const FvrtLists = document.getElementById("fvrtList");
+const fvrt = JSON.parse(localStorage.getItem("fvrt")) || [];
+
+// Set initial height for the FvrtLists element
+FvrtLists.style.minHeight = "200px"; // Adjust the value as needed
 
 selectPerPage.addEventListener("change", () => {
   itemsPerPage = parseInt(selectPerPage.value);
   currentPage = 1; // Reset to the first page when changing items per page
   showCountryList();
 });
+function openCountryModal(name) {
+  const university = countryData.find(
+    (countryItem) => countryItem.name === name
+  );
+
+  // Modify the modal content based on the selected university
+  const modalTitle = document.getElementById("modalTitle");
+  const modalBody = document.getElementById("modalBody");
+
+  // Update the modal title
+  modalTitle.innerText = `University Details: ${university.name}`;
+
+  // Update the modal body with university information
+  modalBody.innerHTML = `
+        <p><strong>University Name:</strong> ${university.name}</p>
+        <p><strong>Country:</strong> ${university.country}</p>
+        <p><strong>State/Province:</strong> ${university["state-province"]}</p>
+      
+      `;
+
+  // Open the modal
+  countryModal.show();
+}
+function openFvrtModal(name) {
+  const university = fvrt.find((countryItem) => countryItem.name === name);
+
+  // Modify the modal content based on the selected university
+  const modalTitle = document.getElementById("modalTitle");
+  const modalBody = document.getElementById("modalBody");
+
+  // Update the modal title
+  modalTitle.innerText = `University Details: ${university.name}`;
+
+  // Update the modal body with university information
+  modalBody.innerHTML = `
+        <p><strong>University Name:</strong> ${university.name}</p>
+        <p><strong>Country:</strong> ${university.country}</p>
+        <p><strong>State/Province:</strong> ${university["state-province"]}</p>
+      
+      `;
+
+  // Open the modal
+  countryModal.show();
+}
+const removeFromFvrt = (countryName) => {
+  const index = fvrt.findIndex((item) => item.name === countryName);
+  if (index !== -1) {
+    fvrt.splice(index, 1);
+    localStorage.setItem("fvrt", JSON.stringify(fvrt));
+    showFvrtList();
+    showCountryList();
+  }
+};
+const moveFvrtUp = (countryName) => {
+  const index = fvrt.findIndex((item) => item.name === countryName);
+  if (index > 0) {
+    const temp = fvrt[index - 1];
+    fvrt[index - 1] = fvrt[index];
+    fvrt[index] = temp;
+    localStorage.setItem("fvrt", JSON.stringify(fvrt));
+    showFvrtList();
+  }
+};
+
+const moveFvrtDown = (countryName) => {
+  const index = fvrt.findIndex((item) => item.name === countryName);
+  if (index < fvrt.length - 1) {
+    const temp = fvrt[index + 1];
+    fvrt[index + 1] = fvrt[index];
+    fvrt[index] = temp;
+    localStorage.setItem("fvrt", JSON.stringify(fvrt));
+    showFvrtList();
+  }
+};
+const moveToTop = (countryName) => {
+  const index = fvrt.findIndex((item) => item.name === countryName);
+  if (index > 0) {
+    const temp = fvrt[index];
+    fvrt.splice(index, 1);
+    fvrt.unshift(temp);
+    localStorage.setItem("fvrt", JSON.stringify(fvrt));
+    showFvrtList();
+  }
+};
+
+const moveToBottom = (countryName) => {
+  const index = fvrt.findIndex((item) => item.name === countryName);
+  if (index < fvrt.length - 1) {
+    const temp = fvrt[index];
+    fvrt.splice(index, 1);
+    fvrt.push(temp);
+    localStorage.setItem("fvrt", JSON.stringify(fvrt));
+    showFvrtList();
+  }
+};
+
+//
+const showFvrtList = () => {
+  FvrtLists.innerHTML =
+    fvrt && fvrt.length > 0
+      ? fvrt
+          .map(
+            (fvrtItem, index) => `
+        <li class="list-group-item px-3">
+          <span class="fw-bold cursor-pointer">${fvrtItem.name} </span>
+          <br />
+          <br />
+          <div class="d-flex justify-content-between">
+            <button type="button" class="btn btn-danger" onclick="removeFromFvrt('${
+              fvrtItem.name
+            }')">
+              <i class="fas fa-trash"></i> Remove
+            </button>
+            <div>
+              ${
+                index !== 0
+                  ? `<button type="button" class="btn btn-secondary" onclick="moveFvrtUp('${fvrtItem.name}')">
+                      <i class="fas fa-arrow-up"></i>
+                    </button>`
+                  : ""
+              }
+              ${
+                index !== fvrt.length - 1
+                  ? `<button type="button" class="btn btn-secondary" onclick="moveFvrtDown('${fvrtItem.name}')">
+                      <i class="fas fa-arrow-down"></i>
+                    </button>`
+                  : ""
+              }
+              ${
+                index !== 0
+                  ? `<button type="button" class="btn btn-secondary" onclick="moveToTop('${fvrtItem.name}')">
+                      Top
+                    </button>`
+                  : ""
+              }
+              ${
+                index !== fvrt.length - 1
+                  ? `<button type="button" class="btn btn-secondary" onclick="moveToBottom('${fvrtItem.name}')">
+                      Bottom
+                    </button>`
+                  : ""
+              }
+              <button type="button" class="btn btn-success" onclick="openFvrtModal('${
+                fvrtItem.name
+              }')">
+                <i class="fas fa-info-circle"></i> Details
+              </button>
+            </div>
+          </div>
+        </li>
+      `
+          )
+          .join("")
+      : "Wishlist is empty";
+
+  // Reset the initial height if the wishlist is not empty
+  if (fvrt && fvrt.length > 0) {
+    FvrtLists.style.minHeight = "auto";
+  }
+};
+
+showFvrtList();
 
 // Event listener for the "Next" button
 document.getElementById("nextBtn").addEventListener("click", () => {
@@ -48,6 +215,20 @@ const getUniversity = async () => {
     console.error("Error fetching universities:", error);
   }
 };
+const isInFavorites = (countryName) => {
+  return fvrt.some((fvrtItem) => fvrtItem.name === countryName);
+};
+const AddToFvrt = (countryItem) => {
+  const fvrtList = fvrt.find((fvrtItem) => fvrtItem.name === countryItem);
+  if (fvrtList) {
+    alert("Already added to favorites");
+  } else {
+    fvrt.push(countryData.find((item) => item.name === countryItem));
+    localStorage.setItem("fvrt", JSON.stringify(fvrt));
+    showFvrtList();
+    showCountryList();
+  }
+};
 
 const showNoUniversitiesMessage = () => {
   countryList.innerHTML = `<li class="list-group-item">No universities found for the provided country.</li>`;
@@ -63,8 +244,17 @@ const showCountryList = () => {
     .map(
       (countryItem) => `
         <li class="list-group-item px-3 d-flex justify-content-between align-items-center">
-          <span class="fw-bold cursor-pointer" onclick="openCountryModal('${countryItem.name}')">${countryItem.name} in</span>
-          <i class="far fa-heart cursor-pointer"></i>
+          <span class="fw-bold cursor-pointer" onclick="openCountryModal('${
+            countryItem.name
+          }')">${countryItem.name} in</span>
+          <i class="far fa-heart cursor-pointer ${
+            isInFavorites(countryItem.name) ? "fav-heart" : ""
+          }" onclick="${
+        isInFavorites(countryItem.name)
+          ? `removeFromFvrt('${countryItem.name}')`
+          : `AddToFvrt('${countryItem.name}')`
+      }"></i>
+
         </li>
       `
     )
@@ -73,30 +263,6 @@ const showCountryList = () => {
   // Remove existing pagination and create new one
   paginationContainer.innerHTML = createPagination();
 };
-
-function openCountryModal(name) {
-  const university = countryData.find(
-    (countryItem) => countryItem.name === name
-  );
-
-  // Modify the modal content based on the selected university
-  const modalTitle = document.getElementById("modalTitle");
-  const modalBody = document.getElementById("modalBody");
-
-  // Update the modal title
-  modalTitle.innerText = `University Details: ${university.name}`;
-
-  // Update the modal body with university information
-  modalBody.innerHTML = `
-        <p><strong>University Name:</strong> ${university.name}</p>
-        <p><strong>Country:</strong> ${university.country}</p>
-        <p><strong>State/Province:</strong> ${university["state-province"]}</p>
-      
-      `;
-
-  // Open the modal
-  countryModal.show();
-}
 
 const createPagination = () => {
   const totalPages = Math.ceil(countryData.length / itemsPerPage);
